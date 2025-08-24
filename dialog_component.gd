bb7217885245d18735fb0009e248e51fea54b570
carsonetb <blackhole243243@gmail.com>
2025-08-24 14:18:39 -0700

    Player scene

--- dialog_component.gd
+++ dialog_component.gd
@@ -1,21 +1,20 @@
 class_name DialogComponent
 extends Area2D
 
-@export var states: Dictionary[String, Conversation]
+@export var states: Dictionary[String, String]
 @export var current_state: String
 
 var interactable: bool = true
+
+signal this_timeline_started
+signal this_timeline_ended
 
 ## Triggers dialog.
 func interact() -> void:
 	if !interactable:
 		return
 	
-	var this_conversation: Conversation = states[current_state]
-	Dialogic.start("res://dialog/timelines/" + this_conversation.timeline_name + ".dtl")
+	this_timeline_started.emit()
+	Dialogic.start("res://dialog/timelines/" + states[current_state] + ".dtl")
 	await Dialogic.timeline_ended
-	
-	if this_conversation.next_state == "end":
-		interactable = false
-		return
-	current_state = this_conversation.next_state
+	this_timeline_ended.emit()
